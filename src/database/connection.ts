fb9cd59c81cf5b979a88b69cbe58f8f456c9fb39
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2021-03-26 20:01:33 -0300

    Conectando mongoDB no servidor

---
+++ src/database/connection.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+class Connection {
+    public mongodbConnection(){
+        return mongoose.connect('mongodb://localhost:27017/db_hackaton_fcamara', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+    }
+} 
+
+export default new Connection;
