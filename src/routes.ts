caad22de610f89a67281a0f45f3abf7853a0a276
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2021-03-29 11:02:20 -0300

    Congurando controller de responsavel com sua model

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import localityRoutes from './routes/LocalityRoutes';
 import schoolRoutes from './routes/SchoolRoutes';
+import responsibleRoutes from './routes/ResponsibleRoutes';
 
 const routes = Router();
 
-routes.use('/api/', localityRoutes, schoolRoutes); 
+routes.use('/api/', localityRoutes, schoolRoutes, responsibleRoutes); 
 routes.use(function(req, res){
     res.send(404);
 });
---
+++ src/routes/ResponsibleRoutes.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+import ResponsibleController from '../controllers/ResponsibleController';
+
+const responsibleRoutes = Router();
+const responsibleController = new ResponsibleController();
+
+responsibleRoutes.post('/responsible/login', responsibleController.login)
+responsibleRoutes.post('/responsible/register', responsibleController.store)
+
+export default responsibleRoutes;
