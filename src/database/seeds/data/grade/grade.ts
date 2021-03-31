944507a66d2cac6e8db726a847b7d3d11d93f7a5
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2021-03-30 23:22:31 -0300

    Inserindo execuçao de seeders de material e serie

---
+++ src/database/seeds/data/grade/grade.ts
@@ -0,0 +1,27 @@
+export = [
+  {
+    "nmGrade": "1º Ano - Fundamental"
+  }, {
+    "nmGrade": "2º Ano - Fundamental"
+  }, {
+    "nmGrade": "3º Ano - Fundamental"
+  }, {
+    "nmGrade": "4º Ano - Fundamental"
+  }, {
+    "nmGrade": "5º Ano - Fundamental"
+  }, {
+    "nmGrade": "6º Ano - Fundamental"
+  }, {
+    "nmGrade": "7º Ano - Fundamental"
+  }, {
+    "nmGrade": "8º Ano - Fundamental"
+  }, {
+    "nmGrade": "9º Ano - Fundamental"
+  }, {
+    "nmGrade": "1º Ano - Ensino médio"
+  }, {
+    "nmGrade": "2º Ano - Ensino médio"
+  }, {
+    "nmGrade": "3º Ano - Ensino médio"
+  }
+]
