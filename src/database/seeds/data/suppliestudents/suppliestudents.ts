4b25826b1ccb1c0d27a0c19b44abbac4735c6da0
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2021-04-05 18:54:56 -0300

    Inserindo seeds e ajustando ids

---
+++ src/database/seeds/data/suppliestudents/suppliestudents.ts
@@ -0,0 +1,25 @@
+import { Types } from 'mongoose';
+
+export =[
+    {
+        "_id": Types.ObjectId('6063f28f1a5b1935080fd093'),
+        "supplie": Types.ObjectId('6063d97436e098f7364f45c5'),
+        "student": Types.ObjectId('6064dc4b3dc13542d41b943a'),
+        "qtSupplie": 3
+    }, {
+        "_id": Types.ObjectId('6063f28f1a5b1932087fd094'),
+        "supplie": Types.ObjectId('6063d97436e098f7364f45c6'),
+        "student": Types.ObjectId('6064dc4b3dc13542d41b943a'),
+        "qtSupplie": 6
+    }, {
+        "_id": Types.ObjectId('6063f28f1a5b1922080fd095'),
+        "supplie": Types.ObjectId('6063d97436e098f7364f45c7'),
+        "student": Types.ObjectId('6064dc4b3dc13542d42b9341'),
+        "qtSupplie": 10
+    }, {
+        "_id": Types.ObjectId('6064df243dc73542d42b9346'),
+        "supplie": Types.ObjectId('6063d97436e098f7364f45c6'),
+        "student": Types.ObjectId('6064dc4b3dc13542d42b9341'),
+        "qtSupplie": 20
+    }
+]
